366f9002cd17b8ec52d6a4d489f6390f67e31174
Jeanette Fairless <jfairless@spectrumhomeservices.com>
2015-06-25 15:28:37 -0700

    answer functionality working

--- app/models/answer.js
+++ app/models/answer.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  riddle: DS.belongsTo('riddle'),
+  riddle: DS.belongsTo('riddle', {async: true}),
   answer_date: DS.attr('string'),
   answer_text: DS.attr('string')
 
--- app/models/riddle.js
+++ app/models/riddle.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  answers: DS.hasMany('answer', {asynch: true}),
+  answers: DS.hasMany('answer', {async: true}),
   riddle_name: DS.attr('string'),
   riddle_text: DS.attr('string')
 });
